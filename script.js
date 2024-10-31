d76e6639c2529c9f63b376e20a18c352fbb8de0a
Dmytro Zhuravlov <ext.dzhuravlov@fgs-uk.com>
2024-10-31 15:47:41 +0100

    prettier

--- script.js
+++ script.js
@@ -16,74 +16,74 @@ var trauma = new Audio("audio/trauma.mp3");
 song = metallica;
 
 function start() {
-    interval = setInterval(render, 1000);
+  interval = setInterval(render, 1000);
 
-    end.onended = function () {
-        count.play();
-    };
-
-    song.onpause = function () {
-        end.play();
-        exerciseCount++;
-        exercise.innerHTML = exercise.innerHTML + ", " + exerciseCount;
-        isNotPausedBefore = false;
-    };
-
+  end.onended = function () {
     count.play();
-    document.getElementsByTagName("button")[0].disabled = true;
-    document.getElementsByTagName("input")[0].disabled = true;
-    document.getElementsByTagName("input")[1].disabled = true;
+  };
+
+  song.onpause = function () {
+    end.play();
+    exerciseCount++;
+    exercise.innerHTML = exercise.innerHTML + ", " + exerciseCount;
+    isNotPausedBefore = false;
+  };
+
+  count.play();
+  document.getElementsByTagName("button")[0].disabled = true;
+  document.getElementsByTagName("input")[0].disabled = true;
+  document.getElementsByTagName("input")[1].disabled = true;
 }
 
 function stop() {
-    song.onpause = function () {
-        end.play();
-    };
+  song.onpause = function () {
+    end.play();
+  };
 
-    end.onended = function () {
-        thanks.play();
-    };
+  end.onended = function () {
+    thanks.play();
+  };
 
-    song.pause();
-    song.currentTime = 0;
-    exerciseCount = 1;
-    clearInterval(interval);
-    isNotPausedBefore = false;
-    exercise.innerHTML = "";
+  song.pause();
+  song.currentTime = 0;
+  exerciseCount = 1;
+  clearInterval(interval);
+  isNotPausedBefore = false;
+  exercise.innerHTML = "";
 
-    document.getElementsByTagName("button")[0].disabled = false;
-    document.getElementsByTagName("input")[0].disabled = false;
-    document.getElementsByTagName("input")[1].disabled = false;
+  document.getElementsByTagName("button")[0].disabled = false;
+  document.getElementsByTagName("input")[0].disabled = false;
+  document.getElementsByTagName("input")[1].disabled = false;
 }
 
 function render() {
-    var time = Math.round(song.currentTime);
-    var seconds = time % 60;
+  var time = Math.round(song.currentTime);
+  var seconds = time % 60;
 
-    current.innerHTML = 60 - seconds;
+  current.innerHTML = 60 - seconds;
 
-    if (seconds == 30 || seconds == 55) {
-        beep.play();
-        isNotPausedBefore = true;
-    }
-    if (time == 5 * 60) {
-        stop();
-        return;
-    }
+  if (seconds == 30 || seconds == 55) {
+    beep.play();
+    isNotPausedBefore = true;
+  }
+  if (time == 5 * 60) {
+    stop();
+    return;
+  }
 
-    if (isNotPausedBefore && seconds == 0) {
-        song.pause();
-    }
+  if (isNotPausedBefore && seconds == 0) {
+    song.pause();
+  }
 }
 
 count.onended = function () {
-    go.play();
+  go.play();
 };
 
 go.onended = function () {
-    song.play();
+  song.play();
 };
 
 thanks.onended = function () {
-    applause.play();
+  applause.play();
 };
